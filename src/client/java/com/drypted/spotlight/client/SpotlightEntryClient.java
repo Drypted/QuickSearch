91853b86bc471f53ef2ce528ba0dd968fef12384
obscure021 <98226994+obscure021@users.noreply.github.com>
2026-01-19 21:45:37 +0500

    Project Setup

---
+++ src/client/java/com/drypted/spotlight/client/SpotlightEntryClient.java
@@ -0,0 +1,12 @@
+package com.drypted.spotlight.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpotlightEntryClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}
---
+++ src/main/java/com/drypted/spotlight/SpotlightEntry.java
@@ -0,0 +1,12 @@
+package com.drypted.spotlight;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpotlightEntry implements ModInitializer
+{
+
+    @Override
+    public void onInitialize()
+    {
+    }
+}
