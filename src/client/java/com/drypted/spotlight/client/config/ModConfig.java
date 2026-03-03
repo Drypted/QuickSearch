8f64a8da2b57038827241b5789a8d6237334b2ff
obscure021 <98226994+obscure021@users.noreply.github.com>
2026-03-03 13:32:16 +0500

    Remove redundant `@ConfigEntry.Gui.Tooltip(count = 1)`

--- src/client/java/com/drypted/spotlight/client/config/ModConfig.java
+++ src/client/java/com/drypted/spotlight/client/config/ModConfig.java
@@ -8,7 +8,7 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 @Config(name = SpotlightEntryClient.MOD_ID)
 public class ModConfig implements ConfigData
 {
-    @ConfigEntry.Gui.Tooltip(count = 1) public boolean enableSpotlight = true;
+    @ConfigEntry.Gui.Tooltip public boolean enableSpotlight = true;
 
     @ConfigEntry.Gui.CollapsibleObject public UI ui = new UI();
     @ConfigEntry.Gui.CollapsibleObject public Hotbar hotbar = new Hotbar();
@@ -19,26 +19,26 @@ public class ModConfig implements ConfigData
     {
 
         @ConfigEntry.BoundedDiscrete(min = 100, max = 800)
-        @ConfigEntry.Gui.Tooltip(count = 1)
+        @ConfigEntry.Gui.Tooltip
         public int searchBarWidth = 200;
         @ConfigEntry.BoundedDiscrete(min = 50, max = 400)
-        @ConfigEntry.Gui.Tooltip(count = 1)
+        @ConfigEntry.Gui.Tooltip
         public int resultsBoxHeight = 100;
     }
 
     public static class Hotbar implements ConfigData
     {
-        @ConfigEntry.Gui.Tooltip(count = 1) public boolean showHotbarHelpText = true;
-        @ConfigEntry.Gui.Tooltip(count = 1) public boolean showHotbarSlots = true;
+        @ConfigEntry.Gui.Tooltip public boolean showHotbarHelpText = true;
+        @ConfigEntry.Gui.Tooltip public boolean showHotbarSlots = true;
     }
 
     public static class Search implements ConfigData
     {
-        @ConfigEntry.Gui.Tooltip(count = 1) public boolean fuzzySearch = true;
-        @ConfigEntry.Gui.Tooltip(count = 1) public boolean caseSensitiveSearch = false;
-        @ConfigEntry.Gui.Tooltip(count = 1) public boolean rememberLastQuery = true;
+        @ConfigEntry.Gui.Tooltip public boolean fuzzySearch = true;
+        @ConfigEntry.Gui.Tooltip public boolean caseSensitiveSearch = false;
+        @ConfigEntry.Gui.Tooltip public boolean rememberLastQuery = true;
         @ConfigEntry.BoundedDiscrete(min = 1, max = 200)
-        @ConfigEntry.Gui.Tooltip(count = 1)
+        @ConfigEntry.Gui.Tooltip
         public int maxResults = 50;
         @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
         @ConfigEntry.Gui.Tooltip(count = 1)
@@ -47,8 +47,8 @@ public class ModConfig implements ConfigData
 
     public static class Notifications implements ConfigData
     {
-        @ConfigEntry.Gui.Tooltip(count = 1) public boolean showItemMessage = true;
-        @ConfigEntry.Gui.Tooltip(count = 1) public boolean showGiveNotifications = true;
+        @ConfigEntry.Gui.Tooltip public boolean showItemMessage = true;
+        @ConfigEntry.Gui.Tooltip public boolean showGiveNotifications = true;
     }
 
     public enum CompletionType
